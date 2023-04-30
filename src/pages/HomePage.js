e73e24a688685ddffc69024a4e623b7fb1ed8a71
primarch93 <sugonegor@gmail.com>
2023-04-30 05:23:57 +0300

    style-2

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,50 @@
 import { Helmet } from 'react-helmet';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  min-height: calc(100vh - 50px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5f5f5;
+  color: #333;
+`;
+
+const Title = styled.h2`
+  font-weight: 500;
+  font-size: 80px;
+  text-align: center;
+  text-shadow: 2px 2px #ccc;
+  position: relative;
+  animation-name: move, pulse;
+  animation-duration: 2s;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
+
+  @keyframes move {
+    0% {
+      transform: translateX(-50%);
+    }
+    50% {
+      transform: translateX(50%);
+    }
+    100% {
+      transform: translateX(-50%);
+    }
+  }
+
+  @keyframes pulse {
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.2);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+`;
 
 export default function HomePage() {
   return (
@@ -6,18 +52,9 @@ export default function HomePage() {
       <Helmet>
         <title>Phonebook</title>
       </Helmet>
-      <div
-        style={{
-          minHeight: 'calc(100vh - 50px)',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <h2 style={{ fontWeight: 500, fontSize: 48, textAlign: 'center' }}>
-          Welcome
-        </h2>
-      </div>
+      <Container>
+        <Title>welcome and have a good time!</Title>
+      </Container>
     </>
   );
 }
